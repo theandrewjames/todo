74bdcec902c38c410fa1f025cdecea4338f0bff1
Andrew <theandrewjames@aol.com>
2016-04-16 14:57:40 -0700

    Completed exercise 122

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var express = require("express");
 var app = express();
+var mongo = require("mongodb");
+var myClient = mongo.MongoClient;
+var url = "mongodb://localhost/test";
+var jsonParser = require("body-parser").json();
+
 
 app.use(express.static("./public"));
+
+app.use(jsonParser);
 
 app.get("/user", function(req, res) {
   var user = {
@@ -12,12 +19,31 @@ app.get("/user", function(req, res) {
 })
 
 app.get("/todos/:user", function(req, res) {
-  if(req.params.user === "Andrew") {
-    var todos = ["Exercise 1","Exercise 2","Exercise 3","Exercise 4"];
-    res.send(todos);
-  } else {
-    res.sendStatus(404);
-  }
+  myClient.connect(url, function(error, database) {
+    if(error) {throw error}
+    else {
+      var exercises = database.collection("exercises");
+      exercises.find({}).toArray(function(error, docs) {
+        res.json(docs);
+        database.close();
+      })
+    }
+  })
+})
+
+app.post("/todos/", jsonParser,  function(req, res) {
+  myClient.connect(url, function(error, database) {
+    if(error) {throw error}
+    else {
+      var exercises = database.collection("exercises");
+      exercises.insert(
+        {thing: req.body.thing}
+      , function(error, results) {
+        res.sendStatus(200);
+        database.close();
+      })
+    }
+  })
 })
 
 app.listen(1337);
--- public/app.js
+++ public/app.js
@@ -16,17 +16,33 @@ function home($http) {
 
 app.controller("todoController", todo);
 
-app.$inject = [$http];
+app.$inject = ["$http"];
 
 function todo($http) {
-  vm = this;
-  var todos = $http.get("http://localhost:1337/todos/Andrew");
-  todos.then(function(todo) {
-    vm.list = todo.data
-  })
+  var vm = this;
+  activate();
+  function activate() {
+    getTodos();
+  }
+  function getTodos() {
+    var todos = $http.get("http://localhost:1337/todos/Andrew");
+    todos.then(function(todo) {
+      vm.list = todo.data;
+    })
+  }
 
   vm.finished = function(item) {
     var position = vm.list.indexOf(item);
     vm.list.splice(position, 1)
+  }
+
+  vm.add = function(item) {
+    var todo = {};
+    todo.thing = item;
+
+    var added = $http.post("http://localhost:1337/todos/", todo);
+    added.then(function() {
+        getTodos();
+    })
   }
 }
