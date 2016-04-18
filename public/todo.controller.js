4b1c4a674a0be5da2dc4eb465f43d65e2da7c37e
Andrew <theandrewjames@aol.com>
2016-04-18 13:57:11 -0700

    Completed exercise 128

---
+++ public/todo.controller.js
@@ -0,0 +1,37 @@
+var app = angular.module("todo");
+
+app.controller("todoController", todo);
+app.$inject = ["$http"];
+
+function todo($http) {
+  var vm = this;
+  activate();
+  function activate() {
+    getTodos();
+  }
+  function getTodos() {
+    var todos = $http.get("http://localhost:1337/todos/Andrew");
+    todos.then(function(todo) {
+      vm.list = todo.data;
+    })
+  }
+
+  vm.finished = function(item) {
+    var finishedItem = item.thing;
+    var removed = $http.delete("http://localhost:1337/todos/delete/" + finishedItem );
+    removed.then(function() {
+      getTodos();
+    })
+  }
+
+  vm.add = function(item) {
+    var todo = {};
+    todo.thing = item;
+
+    var added = $http.post("http://localhost:1337/todos/add/", todo);
+    added.then(function() {
+        getTodos();
+    })
+  }
+
+}
